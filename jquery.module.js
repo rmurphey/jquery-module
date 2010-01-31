63fbb107ce3774f1d2acb042005016ae8cc49f6f
Rebecca Murphey <rebecca@rebeccamurphey.com>
2010-01-30 22:54:48 -0500

    more unobfuscation

--- jquery.module.js
+++ jquery.module.js
@@ -1,52 +1,53 @@
 (function($){
 	$.module = function(
 		moduleName	/* String */, 
-		base 		/* Module(s) to Inherit (String or Array) */, 
-		p 			/* Module Prototype */
+		inherited 		/* Module(s) to Inherit (String or Array) */, 
+		proto 		/* Module Prototype */
 	) {
-		var m = moduleName.split('.'),
-			c = m.pop(),
-			o = window;
+		var moduleNamePieces = moduleName.split('.'),
+			className = moduleNamePieces.pop(),
+			inheritedObject = window;
 			
-		$.each(m, function(i, v){
-			o[v] = o[v] || {};
-			o = o[v];
+		$.each(pieces, function(i, v){
+			inheritedObject[v] = inheritedObject[v] || {};
+			inheritedObject = inheritedObject[v];
 		});
 		
-		o[c] = new Module(p, base, moduleName);
-		o[c].prototype._moduleName = moduleName;
+		o[className] = new Module(proto, inherited, moduleName);
+		o[className].prototype._moduleName = moduleName;
 	};
 
-	var Module = function(p, base, moduleName) {
+	var Module = function(p, inherited, moduleName) {
 		/* 	
 			Thanks to $.widget from jQuery UI for helping to clarify how to do this
 		*/
-		var b = $.isArray(base) ? base : [ base ],
+		var inherited = $.isArray(inherited) ? inherited : [ inherited ],
 			F = function() {
 				$.isFunction(this.init) && this.init.apply(this, arguments);
 				return this;
 			};
 			
-		b = $.map(b.reverse(), function(base, i) {
-			if (base === undefined) {
+		inherited = $.map(inherited.reverse(), function(inherited, i) {
+			if (inherited === undefined) {
 				throw('Missing dependency for ' + moduleName);
 				return null;
 			}
-			var base = $.isFunction(base) ? base.prototype : base;
-			F.prototype = $.extend({}, F.prototype, base);
-			return base;
+			var inherited = $.isFunction(inherited) ? inherited.prototype : inherited;
+			F.prototype = $.extend({}, F.prototype, inherited);
+			return inherited;
 		});
 			
 		F.prototype = $.extend({}, F.prototype, p);
 		
 		F.prototype.inherited = function(method) {
-			if (!b.length) { return; }
-			var inheritedClass = b[b.length-1];
+			if (!inherited.length) { return; }
+			var inheritedClass = inherited[inherited.length-1];
 
-			var fn, s, m = (typeof(method) === 'string' && inheritedClass.prototype[method]);
+			var fn, s, 
+				methodPassed = (typeof(method) === 'string' && inheritedClass.prototype[method]);
 			
-			fn = inheritedClass[m ? method : 'init'];
-			$.isFunction(fn) && fn.apply(this, Array.prototype.slice.call(arguments).slice(m ? 0 : 1));
+			fn = inheritedClass[methodPassed ? method : 'init'];
+			$.isFunction(fn) && fn.apply(this, Array.prototype.slice.call(arguments).slice(methodPassed ? 0 : 1));
 		}
 
 		return F;
